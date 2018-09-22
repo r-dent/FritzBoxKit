c82c1131ff272c6900ef35194b8013d4b681ddcc
Roman Gille <developer@romangille.com>
2018-09-22 23:47:05 +0200

    Display devices in a UITableViewController

--- Fritz!Box Kit/ViewController.swift
+++ Fritz!Box Kit/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import AEXML
 
-class ViewController: UIViewController, URLSessionDelegate {
+class ViewController: UITableViewController {
     
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
     var manager: FritzBox!
+    var devices: [Device] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,16 @@ class ViewController: UIViewController, URLSessionDelegate {
             user: "",
             password: ""
         )
+        
+        manager.login { [weak self] (info, error) in
+            if let error = error {
+                print("Error: \(error)")
+            }
+            else {
+                print("Info: \(String(describing: info))")
+                self?.loadDevices()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,28 +42,39 @@ class ViewController: UIViewController, URLSessionDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func runChallenge(_ sender: Any) {
-        manager.login { (info, error) in
-            if let error = error {
-                print("Error: \(error)")
-            }
-            else {
-                print("Info: \(info)")
-            }
-        }
-    }
-    
-    @IBAction func getDevices(_ sender: Any) {
-        manager.getDevices(completion: { (devices, deviceError) in
+    func loadDevices() {
+        manager.getDevices(completion: { [weak self] (devices, deviceError) in
             if let error = deviceError {
                 print("Device Error: \(error)")
             }
             else {
-                print("Info: \(devices)")
+                self?.devices = devices
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
             }
         })
     }
     
+    // MARK: - UITableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devices.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let device = devices[indexPath.row]
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: "deviceCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "deviceCell")
+        }
+        
+        cell.textLabel?.text = device.displayName
+        cell.detailTextLabel?.text = device.identifier
+        
+        return cell
+    }
 
 }
 
